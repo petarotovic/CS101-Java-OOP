c498ad6fb5cf22b7a3e80dfe02bab781a65c265c
petarotovic <otovicpetar1998@gmail.com>
2023-02-23 00:40:43 -0800

    Week 1, ID

--- MainClass.java
+++
@@ -1,7 +0,0 @@
-public class MainClass {
-    public static void main(String[] args) {
-        System.out.println("Petar");
-        System.out.println("Petar 2");
-        System.out.println("Test");
-    }
-}
---
+++ Nedelja1/IndividualneVezbe.java
@@ -0,0 +1,7 @@
+package Nedelja1;
+
+public class IndividualneVezbe {
+    public static void main(String[] args) {
+        System.out.println("Petar");
+    }
+}
